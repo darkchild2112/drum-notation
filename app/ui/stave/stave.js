14929f6443b64220e56884a925e5eaf7df20a773
Ben Abbott <ben.abbott@intoglobal.com>
2024-01-29 16:08:33 +0000

    added time signature

--- app/ui/stave/stave.js
+++ app/ui/stave/stave.js
@@ -1,8 +1,14 @@
 import styles from "./stave.module.css";
 
 export default function Stave({ children }) {
+  
+  const timeSignature = [4,4];
+  
   return (
     <section className={styles.stave}>
+      <p className={styles.timeSignature}>
+        { timeSignature.map(e => (<span key={e}>{e}</span>)) }
+      </p>
       {children}
     </section>
   )
